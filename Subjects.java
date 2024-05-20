a41a2e4c65394a10f595f9448f56d424903f628b
fizza49 <161933377+fizza49@users.noreply.github.com>
2024-05-20 12:37:32 +0500

    Create Subjects.java

---
+++ Subjects.java
@@ -0,0 +1,13 @@
+//interface that is implemeted by all four subjects
+ public interface Subjects {
+ 
+    void initQuiz();
+    void setDate();
+    void setupTimer();
+    void displayStudentInfo();
+    void loadQuestion();
+    void answerCheck();
+    void submit();
+}
+
+
